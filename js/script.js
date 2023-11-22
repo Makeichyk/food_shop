670203b26cb338121335a616e0e8821f072dd55e
Jaroslav Makeichyk <jaroslau1@gmail.com>
2023-11-22 09:48:27 +0100

    feat: update slider, add navigation points

--- js/script.js
+++ js/script.js
@@ -300,61 +300,113 @@ window.addEventListener("DOMContentLoaded", () => {
 
   // SLIDER
 
-  const sliderWrapper = document.querySelector(".offer__slider-wrapper"),
+  const slider = document.querySelector(".offer__slider"),
+    sliderWrapper = slider.querySelector(".offer__slider-wrapper"),
     sliderInner = sliderWrapper.querySelector(".offer__slider-inner"),
     slides = sliderInner.querySelectorAll(".offer__slide"),
     slidesCount = slides.length,
-    sliderCouner = document.querySelector(".offer__slider-counter"),
+    sliderCouner = slider.querySelector(".offer__slider-counter"),
     arrowPrev = sliderCouner.querySelector(".offer__slider-prev"),
     arrowNext = sliderCouner.querySelector(".offer__slider-next"),
     total = sliderCouner.querySelector("#total"),
     current = sliderCouner.querySelector("#current"),
     wrapperWidth = window.getComputedStyle(sliderWrapper).width,
-    numWrapperWidth = +wrapperWidth.slice(0, wrapperWidth.length - 2);
+    numWrapperWidth = +wrapperWidth.slice(0, wrapperWidth.length - 2),
+    dotsArray = [];
 
   let slideIndex = 1,
     offset = 0;
 
   sliderInner.style.width = `${100 * slides.length}%`;
+
   slides.forEach((slide) => (slide.style.width = wrapperWidth));
 
-  current.textContent = `0${slideIndex}`;
-  if (slidesCount < 10) {
-    total.textContent = `0${slidesCount}`;
-  } else {
-    total.textContent = slidesCount;
+  function drawNavigationDots() {
+    slider.style.position = "relative";
+
+    const dots = document.createElement("ol");
+
+    dots.classList.add("carousel-dots");
+    slider.append(dots);
+
+    for (let i = 0; i < slides.length; i++) {
+      const dot = document.createElement("li");
+      dot.setAttribute("data-slide-to", i + 1);
+      dot.classList.add("dot");
+      dots.append(dot);
+      dotsArray.push(dot);
+    }
+
+    dotsArray[slideIndex - 1].classList.add("dot-active");
+  }
+
+  function checkValueOfCurIndex(index) {
+    index < 10
+      ? (current.textContent = `0${index}`)
+      : (current.textContent = index);
+  }
+
+  function checkvalueOfTotal(count) {
+    count < 10
+      ? (total.textContent = `0${slidesCount}`)
+      : (total.textContent = slidesCount);
   }
 
   function slide(direction) {
     if (direction.toLowerCase() === "next") {
+      dotsArray[slideIndex - 1].classList.remove("dot-active");
+
       +offset === numWrapperWidth * (slides.length - 1)
         ? (offset = 0)
         : (offset += numWrapperWidth);
 
       slideIndex == slides.length ? (slideIndex = 1) : slideIndex++;
 
-      slideIndex < 10
-        ? (current.textContent = `0${slideIndex}`)
-        : (current.textContent = slideIndex);
+      checkValueOfCurIndex(slideIndex);
 
       sliderInner.style.transform = `translateX(-${offset}px)`;
+
+      dotsArray[slideIndex - 1].classList.add("dot-active");
     } else if (direction.toLowerCase() === "prev") {
+      dotsArray[slideIndex - 1].classList.remove("dot-active");
+
       offset === 0
         ? (offset = numWrapperWidth * (slides.length - 1))
         : (offset -= numWrapperWidth);
 
       slideIndex == 1 ? (slideIndex = slides.length) : slideIndex--;
 
-      slideIndex < 10
-        ? (current.textContent = `0${slideIndex}`)
-        : (current.textContent = slideIndex);
+      checkValueOfCurIndex(slideIndex);
 
       sliderInner.style.transform = `translateX(-${offset}px)`;
+
+      dotsArray[slideIndex - 1].classList.add("dot-active");
     } else {
       console.error("wrong function parameters");
     }
   }
 
+  checkValueOfCurIndex(slideIndex);
+  drawNavigationDots();
+  checkvalueOfTotal(slidesCount);
+
   arrowNext.addEventListener("click", () => slide("next"));
   arrowPrev.addEventListener("click", () => slide("prev"));
+  dotsArray.forEach((dot) =>
+    dot.addEventListener("click", (e) => {
+      const slideTo = e.target.getAttribute("data-slide-to");
+      dotsArray[slideIndex - 1].classList.remove("dot-active");
+
+      slideIndex = slideTo;
+      offset = numWrapperWidth * (slideTo - 1);
+      sliderInner.style.transform = `translateX(-${offset}px)`;
+
+      dotsArray[slideIndex - 1].classList.add("dot-active");
+
+      checkValueOfCurIndex(slideIndex);
+    })
+  );
 });
+
+//  Почему я не получаю undefiend когда использую querySelector по отношению к элементам,
+//  которые появлятся в DOM только после вызова функции, которая вызывается после вызовов querySelector ?
